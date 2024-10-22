aec1e8593577b8888d633fbe8955e310777bce7f
fazlullinhub <164046064+fazlullinhub@users.noreply.github.com>
2024-10-22 09:23:54 +0300

    Add files via upload

---
+++ ConsoleApp42/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp42
+{
+    public class Animal
+    {
+        public virtual void Sound()
+        {
+            Console.WriteLine("Animal sound");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("Woof");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myDog = new Dog();
+            Animal myCat = new Cat();
+
+            myDog.Sound();
+            myCat.Sound();
+        }
+    }
+}
